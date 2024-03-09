52da789c7c9c5e97df77a8e28a450acd556cbcbc
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2024-03-10 02:55:50 +0800

    .

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
- 
 # Function to generate README content
 def generate_readme(data):
-    return f'''<div align="center">
+    return '''<div align="center">
 
-# {data['title']} 
+# {} 
 
-[![GitHub license](https://img.shields.io/github/license/{data['username']}/{data['repository']})](https://github.com/{data['username']}/{data['repository']}/blob/main/LICENSE)
-[![GitHub stars](https://img.shields.io/github/stars/{data['username']}/{data['repository']})](https://github.com/{data['username']}/{data['repository']}/stargazers)
-[![GitHub issues](https://img.shields.io/github/issues/{data['username']}/{data['repository']})](https://github.com/{data['username']}/{data['repository']}/issues)
+[![GitHub license](https://img.shields.io/github/license/{}/{})](https://github.com/{}/{}blob/main/LICENSE)
+[![GitHub stars](https://img.shields.io/github/stars/{}/{}?style=flat)](https://github.com/{}/{}stargazers)
+[![GitHub issues](https://img.shields.io/github/issues/{}/{}?style=flat)](https://github.com/{}/{}issues)
 
 </div>
 
-{data['description']}
+{}
 
 ## Features
 
-{'\n'.join([f"- {feature}" for feature in data['features']])}
+{}
 
 ## Technologies Used
 
-{'\n'.join([f"- {tech}" for tech in data['technologies']])}
+{}
 
 ## Getting Started
 
-{data['getting_started']}
+{}
 
 ## Usage
 
-{data['usage']}
+{}
 
 ## Contribution
 
-{data['contribution']}
+{}
 
 ## License
 
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
-'''
+'''.format(data['title'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['description'], '\n'.join([f"- {feature}" for feature in data['features']]), '\n'.join([f"- {tech}" for tech in data['technologies']]), data['getting_started'], data['usage'], data['contribution'])
 
 # Function to write README file
+
+
 def write_readme(content):
     with open('README.md', 'w') as f:
         f.write(content)
     print('README.md file created successfully!')
-
 # Function to collect user input
+
+
 def collect_input():
     data = {
         'username': input('Enter your GitHub username: '),
@@ -60,10 +62,13 @@ def collect_input():
     return data
 
 # Main function
+
+
 def main():
     data = collect_input()
     readme_content = generate_readme(data)
     write_readme(readme_content)
+
 
 if __name__ == "__main__":
     main()

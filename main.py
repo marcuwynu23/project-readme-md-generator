b1b2fb2bd3123424ed72c6c959357b7f52fa6ebc
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2024-03-10 02:56:39 +0800

    added: license prompt

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def generate_readme(data):
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
-'''.format(data['title'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['description'], '\n'.join([f"- {feature}" for feature in data['features']]), '\n'.join([f"- {tech}" for tech in data['technologies']]), data['getting_started'], data['usage'], data['contribution'])
+This project is licensed under the {} License - see the [LICENSE](LICENSE) file for details.
+'''.format(data['title'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['username'], data['repository'], data['description'], '\n'.join([f"- {feature}" for feature in data['features']]), '\n'.join([f"- {tech}" for tech in data['technologies']]), data['getting_started'], data['usage'], data['contribution'], data['license'])
 
 # Function to write README file
 
@@ -44,6 +44,7 @@ def write_readme(content):
     with open('README.md', 'w') as f:
         f.write(content)
     print('README.md file created successfully!')
+
 # Function to collect user input
 
 
@@ -57,7 +58,8 @@ def collect_input():
         'technologies': input('Enter technologies used (separated by commas): ').split(','),
         'getting_started': input('Enter getting started guide: '),
         'usage': input('Enter usage instructions: '),
-        'contribution': input('Enter contribution guidelines: ')
+        'contribution': input('Enter contribution guidelines: '),
+        'license': input('Enter project license: ')
     }
     return data
 
